58387de6bdaa06dd2c598616cf1de3c5bf891e3a
Brian Rono <brishadow24@gmail.com>
2022-08-04 00:01:46 +0300

    The old way

---
+++ main.js
@@ -0,0 +1,22 @@
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    isVisible: true,
+    draw: function () {
+        console.log('draw')
+    }
+}
+const circle2 = {
+    radius: 1,
+    location: {    ///this is hecticccc!!!
+        x: 2,
+        y: 2
+    },
+    isVisible: true,
+    draw: function () {
+        console.log('draw')
+    }
+}
